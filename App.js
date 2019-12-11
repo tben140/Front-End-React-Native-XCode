6dd01827a4ef6700bbd7d4864332c04e9bacc507
tben140 <19746934+tben140@users.noreply.github.com>
2019-12-11 14:14:19 +0000

    Add findCoordinates functionality (#7)

--- App.js
+++ App.js
@@ -15,6 +15,10 @@ import {
   View,
   Text,
   StatusBar,
+  Platform,
+  Alert,
+  TouchableOpacity,
+  PermissionsAndroid,
 } from 'react-native';
 
 import {
@@ -25,11 +29,63 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Geolocation from 'react-native-geolocation-service';
+
 import Map from './src/Map';
 import Homepage from './src/Homepage';
 
 class App extends Component {
+  state = {
+    latitude: null,
+    longitude: null,
+  };
+
+  findCoordinates = () => {
+    console.log('Find coordinates running');
+    Geolocation.getCurrentPosition(
+      location => {
+        console.log(location.coords.latitude, location.coords.longitude);
+        this.setState({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        });
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  };
+
+  componentDidMount() {
+    async function requestLocationPermission() {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Location Permission',
+            message: 'Running App needs access to your camera ',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('Location Permission Granted');
+        } else {
+          console.log('Location permission denied');
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    }
+    requestLocationPermission();
+  }
+
   render() {
+    {
+      this.state.latitude === null && this.findCoordinates();
+    }
     return (
       <>
         {/* <Map /> */}
